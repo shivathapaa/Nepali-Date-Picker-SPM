949e4b0590273533fe8c926f36a37508684c2741
Shiva Thapa <shivathapa0600@gmail.com>
2025-04-06 23:11:10 +0545

    Release of version 2.3.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "nepali-date-picker",
-            url: "https://github.com/shivathapaa/Nepali-Date-Picker/releases/download/2.3.0/nepali_date_picker.xcframework.zip",
-            checksum: "323a3afaaf2b5fc724e1efe325aab03816afea2b67f2165cce5404a988ec3b69"
+            url: "https://github.com/shivathapaa/Nepali-Date-Picker/releases/download/2.3.1/nepali_date_picker.xcframework.zip",
+            checksum: "a7808195dc70ada31e65b1d40d65b017a21d30d0a6c60f74fac833cd4e5b7fe6"
         )
     ]
 )
